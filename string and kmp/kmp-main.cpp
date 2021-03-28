3e759cc2dea92f15ed94990e0cde02d3eabad78e
Harrry-Cao <2499664180@qq.com>
2021-03-28 12:50:47 +0800

    add a file about string.cpp

---
+++ string and kmp/kmp-main.cpp
@@ -0,0 +1,24 @@
+/**kmp主函数
+ * 整体的思路可以参考    kmp-next.cpp
+ * */
+# include"kmp-next.cpp"
+
+int kmp(SqString t,SqString orin)
+{
+    int next[MaxSize];
+    getNext(t, next);
+    //开始比较
+    int i = 0, j = 0;//初始化指针，i指向主串，j指向子串
+    while(i<orin.length&&j<t.length)
+    {
+        if(orin.data[i]==t.data[j])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j = next[j];
+        }
+    }
+}
---
+++ string and kmp/string.cpp
@@ -0,0 +1,17 @@
+#define MaxSize 100
+#include<stdio.h>
+#include<string.h>
+
+
+//初始化串
+void init(){
+    #
+}
+
+
+//销毁串
+void destroy(){
+    #
+}
+
+//
